688e0644fee8fe0cf478d29cc2087b5a3639fdda
Rajat Sardesai <rajatsardesai@gmail.com>
2021-06-02 14:57:04 +0530

    Initial Commit

--- Demo React Website/src/App.js
+++ Demo React Website/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Todo } from './components/Todo';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './App.css';
+import { Navbar } from './Navbar';
+import { Home } from './Home';
+import { About } from './About';
+import { Service } from './Service';
+import { Contact } from './Contact';
+import { Footer } from './Footer';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <>
-      <Todo />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/service' component={Service} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect to='/' />
+      </Switch>
+      <Footer/>
     </>
   )
 }
--- Demo React Website/src/index.js
+++ Demo React Website/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </>,
   document.getElementById('root')
 );
