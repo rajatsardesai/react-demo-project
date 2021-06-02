7f8d9d2e0b11e3738e66d94718854828fde008c0
Rajat Sardesai <rajatsardesai@gmail.com>
2021-06-02 13:09:45 +0530

    Add files via upload

---
+++ Redux Todo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Todo } from './components/Todo';
+
+export const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  )
+}
